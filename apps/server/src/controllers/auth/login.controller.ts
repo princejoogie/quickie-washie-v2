3e199d277e9035151a5b77486fbe2f65fd99c2fa
princejoogie <princejoogie@gmail.com>
2022-09-07 17:06:30 +0800

    feat: return access token in login

--- apps/server/src/controllers/auth/login.controller.ts
+++ apps/server/src/controllers/auth/login.controller.ts
@@ -1,14 +1,44 @@
 import type { RequestHandler } from "express";
+import bcrypt from "bcryptjs";
+
+import prisma from "../../lib/prisma";
 import { AppError } from "../../utils/error";
+import { createAndRefreshToken } from "../../utils/jwt-helper";
 import type { LoginResponse, LoginBody } from "../../dtos/auth.dto";
 
 const loginController: RequestHandler<any, LoginResponse, LoginBody> = async (
-  _,
+  req,
   res,
   next
 ) => {
   try {
-    return res.status(200).json({ accessToken: "access_token_here" });
+    const { email, password } = req.body;
+
+    const user = await prisma.user.findFirst({
+      where: { email },
+    });
+
+    if (!user) {
+      const error = new AppError("NotFoundException", "User does not exist");
+      return next(error);
+    }
+
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if (!isValid) {
+      const error = new AppError(
+        "UnauthorizedException",
+        "Invalid credentials"
+      );
+      return next(error);
+    }
+
+    const accessToken = createAndRefreshToken(
+      { id: user.id, privilege: user.privilege },
+      res
+    );
+
+    return res.status(200).json({ accessToken });
   } catch (e) {
     const error = new AppError(
       "InternalServerErrorException",
---
+++ apps/server/src/utils/jwt-helper.ts
@@ -0,0 +1,48 @@
+import type { Response } from "express";
+import jwt from "jsonwebtoken";
+
+export interface TokenPayload {
+  id: string;
+  privilege: string;
+}
+
+export const HASH_SALT = 10;
+
+export const REFRESH_TOKEN_KEY = "refreshToken";
+
+export const verifyAccessToken = (token: string): TokenPayload => {
+  return jwt.verify(token, process.env["ACCESS_TOKEN_SECRET"]!) as TokenPayload;
+};
+
+export const verifyRefreshToken = (refreshToken: string): TokenPayload => {
+  return jwt.verify(
+    refreshToken,
+    process.env["REFRESH_TOKEN_SECRET"]!
+  ) as TokenPayload;
+};
+
+export const createAccessToken = (payload: TokenPayload) => {
+  return jwt.sign(payload, process.env["ACCESS_TOKEN_SECRET"]!, {
+    expiresIn: "10m",
+    algorithm: "HS256",
+  });
+};
+
+export const createRefreshToken = (payload: TokenPayload) => {
+  return jwt.sign(payload, process.env["REFRESH_TOKEN_SECRET"]!, {
+    expiresIn: "20d",
+    algorithm: "HS256",
+  });
+};
+
+export const createAndRefreshToken = (payload: TokenPayload, res: Response) => {
+  const accessToken = createAccessToken(payload);
+  const refreshToken = createRefreshToken(payload);
+
+  res.cookie(REFRESH_TOKEN_KEY, refreshToken, {
+    httpOnly: true,
+    sameSite: "lax",
+  });
+
+  return accessToken;
+};
