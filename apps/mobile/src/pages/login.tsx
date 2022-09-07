f10d562ea7a3676cf139da916206f6a4b6fe53a5
princejoogie <princejoogie@gmail.com>
2022-09-07 13:39:12 +0800

    etc: test

--- apps/mobile/src/pages/login.tsx
+++ apps/mobile/src/pages/login.tsx
@@ -5,6 +5,7 @@ import { RootStackParamList } from "./types";
 import { Layout } from "../components";
 import { logOut, signIn } from "../firebase";
 import { useAuthContext } from "../contexts/auth-context";
+import { api } from "../services/api";
 
 export const Login = ({}: NativeStackScreenProps<
   RootStackParamList,
@@ -20,14 +21,12 @@ export const Login = ({}: NativeStackScreenProps<
       <Text className="w-full text-2xl font-bold text-white">
         Welcome to the app! {user?.email}
       </Text>
-
       <TextInput
         placeholder="Email"
         value={email}
         onChangeText={setEmail}
         className="border-gray-700 mt-6 rounded border-2 px-4 py-3 text-white"
       />
-
       <TextInput
         placeholder="Password"
         secureTextEntry
@@ -35,6 +34,24 @@ export const Login = ({}: NativeStackScreenProps<
         onChangeText={setPassword}
         className="border-gray-700 mt-6 rounded border-2 px-4 py-3 text-white"
       />
+
+      <TouchableOpacity
+        className="bg-gray-700 mt-2 self-start rounded px-4 py-2"
+        onPress={async () => {
+          try {
+            const res = await api.get(
+              "https://jsonplaceholder.typicode.com/todos/1"
+            );
+            Alert.alert("Success", JSON.stringify(res.data));
+          } catch (error) {
+            const err = error as any;
+            Alert.alert("Error", JSON.stringify(err.message));
+            console.log(JSON.stringify(error));
+          }
+        }}
+      >
+        <Text className="text-white">Test api</Text>
+      </TouchableOpacity>
 
       {!user ? (
         <TouchableOpacity
