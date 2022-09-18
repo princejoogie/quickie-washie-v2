686a48d5e8dee6fc48c25ef97b12806a32df4f03
princejoogie <princejoogie@gmail.com>
2022-09-18 17:48:03 +0800

    feat(server): get appointment by id
    new file:   apps/server/src/controllers/appointment/get-by-id.controller.ts
    modified:   apps/server/src/controllers/service/get-by-id.controller.ts
    modified:   apps/server/src/routes/appointment.ts

---
+++ apps/server/src/controllers/appointment/get-by-id.controller.ts
@@ -0,0 +1,53 @@
+import type { RequestHandler } from "express";
+import type {
+  GetAppointmentByIdParams,
+  GetAppointmentByIdResponse,
+} from "@qw/dto";
+
+import prisma from "../../lib/prisma";
+import { AppError, handleControllerError } from "../../utils/error";
+
+const getAppointmentByIdController: RequestHandler<
+  GetAppointmentByIdParams,
+  GetAppointmentByIdResponse
+> = async (req, res, next) => {
+  try {
+    const payload = req.tokenPayload;
+
+    if (!payload) {
+      const error = new AppError("UnauthorizedException", "No token provided");
+      return next(error);
+    }
+
+    const { appointmentId } = req.params;
+
+    const appointment = await prisma.appointment.findUnique({
+      where: { id: appointmentId },
+      include: {
+        AdditionalPrice: true,
+        Service: true,
+        Vehicle: true,
+        User: {
+          select: {
+            id: true,
+            email: true,
+            name: true,
+            licenseUrl: true,
+            photoUrl: true,
+          },
+        },
+      },
+    });
+
+    if (!appointment) {
+      const error = new AppError("NotFoundException", "Appointment not found");
+      return next(error);
+    }
+
+    return res.status(200).json(appointment);
+  } catch (e) {
+    handleControllerError(e, next);
+  }
+};
+
+export default getAppointmentByIdController;
