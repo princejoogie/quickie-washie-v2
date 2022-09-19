cf0fe8adb9930d5122866c6ada0a06ade9069181
princejoogie <princejoogie@gmail.com>
2022-09-19 23:17:57 +0800

    chore(all): create base tsconfig
    modified:   apps/mobile/src/pages/dashboard/customer/appointments/index.tsx
    new file:   apps/mobile/src/pages/dashboard/customer/appointments/types.ts
    modified:   apps/mobile/src/pages/dashboard/customer/index.tsx
    modified:   apps/server/tsconfig.json
    modified:   packages/db/package.json
    modified:   packages/db/tsconfig.json
    modified:   packages/dto/package.json
    modified:   packages/dto/tsconfig.json
    new file:   tsconfig.base.json

--- apps/mobile/src/pages/dashboard/customer/appointments/index.tsx
+++ apps/mobile/src/pages/dashboard/customer/appointments/index.tsx
@@ -1,10 +1,63 @@
+import { Text, View } from "react-native";
+import { useQuery } from "@tanstack/react-query";
+import { useIsFocused } from "@react-navigation/native";
 import { BottomTabScreenProps } from "@react-navigation/bottom-tabs";
-import { Layout } from "../../../../components";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+
+import {
+  CustomerAppointmentsStack,
+  CustomerAppointmentsStackParamList,
+} from "./types";
+
 import { CustomerDashboardParamList } from "../types";
+
+import { Layout } from "../../../../components";
+import appointmentService from "../../../../services/appointment";
 
 export const Appointments = ({}: BottomTabScreenProps<
   CustomerDashboardParamList,
   "Appointments"
 >) => {
-  return <Layout nav={{ title: "Appointments" }}>{null}</Layout>;
+  return (
+    <CustomerAppointmentsStack.Navigator
+      screenOptions={{
+        headerShown: false,
+        contentStyle: {
+          backgroundColor: "#111827",
+        },
+      }}
+      initialRouteName="AllAppointments"
+    >
+      <CustomerAppointmentsStack.Screen
+        name="AllAppointments"
+        component={AllAppointments}
+      />
+    </CustomerAppointmentsStack.Navigator>
+  );
+};
+
+const AllAppointments = ({}: NativeStackScreenProps<
+  CustomerAppointmentsStackParamList,
+  "AllAppointments"
+>) => {
+  const appointments = useQuery(["appointments"], appointmentService.getAll);
+  const isFocused = useIsFocused();
+
+  if (isFocused && appointments.isStale) {
+    appointments.refetch();
+  }
+
+  return (
+    <Layout nav={{ title: "Appointments" }}>
+      {appointments.isLoading ? (
+        <Text>Loading...</Text>
+      ) : (
+        appointments.data?.map((appointment) => (
+          <View key={appointment.id}>
+            <Text className="text-white">{appointment.id}</Text>
+          </View>
+        ))
+      )}
+    </Layout>
+  );
 };
