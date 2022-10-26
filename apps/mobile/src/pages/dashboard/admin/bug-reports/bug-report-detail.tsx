c1c3fedcbd194102b5599b1557b8baacaf9c067e
princejoogie <princejoogie@gmail.com>
2022-10-26 21:57:01 +0800

    feat: bug-report-detail
    modified:   apps/mobile/src/pages/dashboard/admin/bug-reports/bug-report-detail.tsx

--- apps/mobile/src/pages/dashboard/admin/bug-reports/bug-report-detail.tsx
+++ apps/mobile/src/pages/dashboard/admin/bug-reports/bug-report-detail.tsx
@@ -1,7 +1,9 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { useQuery } from "@tanstack/react-query";
-import { Text } from "react-native";
-import { Layout } from "../../../../components";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import * as WebBrowser from "expo-web-browser";
+
+import { Layout, LoadingText, TextField } from "../../../../components";
 import reportService from "../../../../services/report";
 import { BugReportStackParamList } from "./types";
 
@@ -24,7 +26,43 @@ export const BugReportDetail = ({
       }}
       onRefresh={report.refetch}
     >
-      <Text className="text-white">{JSON.stringify(report.data, null, 2)}</Text>
+      {report.isLoading ? (
+        <LoadingText />
+      ) : !!report.data ? (
+        <>
+          <TextField editable={false} label="Title" value={report.data.title} />
+
+          <TextField
+            editable={false}
+            multiline
+            label="Body"
+            value={report.data.body}
+          />
+
+          <Text className="mt-4 ml-2 text-xs text-gray-400">Screenshots</Text>
+          {report.data.screenshotUrls.length > 0 && (
+            <View className="mt-1 flex min-h-[128px] flex-row flex-wrap rounded-lg border-2 border-transparent bg-gray-800 p-2 text-white opacity-100">
+              {report.data.screenshotUrls.map((uri, idx) => (
+                <TouchableOpacity
+                  key={`screenshot-${idx}`}
+                  className="relative m-1 h-40 w-[31%] rounded border-2 border-gray-700"
+                  onPress={() => {
+                    WebBrowser.openBrowserAsync(uri);
+                  }}
+                >
+                  <Image
+                    source={{ uri }}
+                    className="h-full w-full"
+                    resizeMode="contain"
+                  />
+                </TouchableOpacity>
+              ))}
+            </View>
+          )}
+        </>
+      ) : (
+        <Text>Report not found</Text>
+      )}
     </Layout>
   );
 };
